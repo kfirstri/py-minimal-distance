c6e8ff7fcea82be373f1e5457569b9a4d6cb717f
Kfir Stri <kfirstar@gmail.com>
2017-04-06 21:12:54 +0300

    redactores get_separated_string to use reqex

--- minimal.py
+++ minimal.py
@@ -5,45 +5,17 @@
 """
 import re
 
-SENTENCE_SEPARATORS = ['.', '!!!']
-
-def get_next_seperator_index(text, start, separators):
-    """
-        Get the location of the next seperator in the text (starting at start),
-        return -1 if none found
-    """
-
-    locations = []
-    for sep in separators:
-        curr_location = text.find(sep, start)
-        if curr_location != -1:
-            locations.append((curr_location, len(sep)))
-
-    # If there were no locations found return -1
-    if len(locations) == 0:
-        return -1
-
-    return min(locations)
-
 def get_separated_strings(text, separators):
     """ Return list of sentences from a text """
     sentences = []
-    curr_index = 0
+    parsed_text = re.split("(\.|!!!)", text.strip())
 
-    sep_tuple = get_next_seperator_index(text, 0, separators)
-    while sep_tuple != -1:
-        # Parse the recieved tuple for location and length
-        # and calculate the new location for the substring
-        sep_loc = sep_tuple[0]
-        sep_len = sep_tuple[1]
-        new_location = sep_loc + sep_len + 1
+    index = 0
+    max_length = len(parsed_text)
 
-        # Save the sentence to the list without extra spaces.
-        sentences.append(text[curr_index:new_location].strip())
-
-        # Find the next sentence
-        curr_index = new_location
-        sep_tuple = get_next_seperator_index(text, curr_index, separators)
+    while index + 1 < max_length:
+        sentences.append(parsed_text[index].strip() + parsed_text[index + 1].strip())
+        index += 2
 
     return sentences
 
@@ -79,7 +51,7 @@ def get_distances(sentences, phrase1, phrase2, neg):
         dist = get_distance_between_phrases(sentence, phrase1, phrase2, neg)
         if dist != -1:
             distances.append(dist)
-    
+
     return distances
 
 def main():
